cd54a039508c7dae74ccda4d222a1eb5e01d536f
willzittlau <67138318+willzittlau@users.noreply.github.com>
2020-09-20 14:45:43 -0700

    add list items functionality

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
 // Define UI Variables
-const form = document.querySelector('#item-form');
-const itemlist = document.querySelector('.collection');
-const clearBtn =document.querySelector('.clear-items');
-const filter = document.querySelector('#filter');
-const itemInput = document.querySelector('#item');
+const form = document.querySelector("#item-form");
+const itemList = document.querySelector(".collection");
+const clearBtn = document.querySelector(".clear-items");
+const filter = document.querySelector("#filter");
+const itemInput = document.querySelector("#item");
 
-// Load Event Listners
+// Load Event Listeners
 loadEventListeners();
-
 function loadEventListeners() {
   // Add item event
-  form.addEventListener('submit', addItem);
+  form.addEventListener("submit", addItem);
 }
 
 //addItem
 function addItem(e) {
-  if (itemInput.value === '') {
-    alert('Empty Input');
+  // Create li item
+  const li = document.createElement("li");
+  li.className = "collection-item";
+  li.appendChild(document.createTextNode(itemInput.value));
+  // Create delete button
+  const link = document.createElement("a");
+  link.className = "delete-item secondary-content";
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  // Add to list
+  li.appendChild(link);
+  itemList.appendChild(li);
+  // Empty submission
+  if (itemInput.value === "") {
+    alert("Empty Input");
+    itemList.lastChild.remove();
   }
+  // Clear Input
+  itemInput.value = "";
+  // Prevent form submission to allow for client side changes
   e.preventDefault();
 }
