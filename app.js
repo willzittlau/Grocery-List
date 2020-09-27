98d5bca9588eb909ad86caed45e87ba575ffb378
willzittlau <67138318+willzittlau@users.noreply.github.com>
2020-09-26 22:06:21 -0700

    Add clear button fucntionality

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ const itemList = document.querySelector(".collection");
 const clearBtn = document.querySelector(".clear-items");
 const filter = document.querySelector("#filter");
 const itemInput = document.querySelector("#item");
-const label = document.querySelector('label');
+const label = document.querySelector("label");
 
 // Load Event Listeners
 loadEventListeners();
 
 function loadEventListeners() {
-  // Add item event
   form.addEventListener("submit", addItem);
-  itemList.addEventListener("click", removeItem)
-  itemList.addEventListener("click", markComplete)
+  itemList.addEventListener("click", removeItem);
+  itemList.addEventListener("click", markComplete);
+  clearBtn.addEventListener("click", clearComplete);
 }
 
 //addItem
@@ -21,25 +21,25 @@ function addItem(e) {
   // Create li item
   const li = document.createElement("li");
   li.className = "collection-item";
-  li.style.cursor = 'pointer';
   li.appendChild(document.createTextNode(itemInput.value));
   // Create delete button
   const link = document.createElement("a");
   link.className = "delete-item secondary-content";
   link.innerHTML = '<i class="fa fa-remove"></i>';
-  link.style.color = 'red';
+  link.style.color = "red";
+  link.style.cursor = "pointer";
   // Add to list
   li.appendChild(link);
   itemList.appendChild(li);
   // Reset input text from error message
-  if (label.innerHTML === 'Input cannot be empty') {
-    label.innerHTML = 'New Item';
-    label.className= 'normal';
+  if (label.innerHTML === "Input cannot be empty") {
+    label.innerHTML = "New Item";
+    label.className = "normal";
   }
   // Empty submission error handling
   if (itemInput.value === "") {
-    label.innerHTML = 'Input cannot be empty';
-    label.className= 'errorMessage';
+    label.innerHTML = "Input cannot be empty";
+    label.className = "errorMessage";
     itemList.lastChild.remove();
   }
   // Clear Input
@@ -48,15 +48,22 @@ function addItem(e) {
   e.preventDefault();
 }
 
-function removeItem (e) {
-  if (e.target.parentElement.classList.contains('delete-item')) {
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains("delete-item")) {
     e.target.parentElement.parentElement.remove();
   }
 }
 
-function markComplete (e) {
-  if (e.target.classList.contains('collection-item')) {
-    e.target.classList.toggle('completed');
-    console.log(e.target.className);
+function markComplete(e) {
+  if (e.target.classList.contains("collection-item")) {
+    e.target.classList.toggle("completed");
   }
+}
+
+function clearComplete() {
+  let completed = document.querySelectorAll(".completed");
+  completed = Array.from(completed);
+  completed.forEach(function (complete) {
+    complete.remove();
+  });
 }
